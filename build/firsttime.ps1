c5b46aa059f2abbb3333e777f1f050cc497e7171
AdrianoCahete <nano.rj@gmail.com>
2015-09-26 20:05:51 -0300

    Better Install script

--- build/firsttime.ps1
+++ build/firsttime.ps1
@@ -5,29 +5,78 @@ Param(
   [string]$database
 )
 
-#create database
-Write-Host "Creating database..."
+# Loading Assemblies
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo") | Out-Null
 
-$srv = new-Object Microsoft.SqlServer.Management.Smo.Server($serviceInstance)
-$db = New-Object Microsoft.SqlServer.Management.Smo.Database($srv, $database)
-$db.Create()
-Write-Host $db.CreateDate
+#Vars
+$error.clear()
 
-# Execute sql initial script
-pushd ..\lib\Test-databases
-Invoke-sqlcmd -inputfile ".\roadkill-sqlserver.sql" -serverinstance $serviceInstance -database $database # the parameter -database can be omitted based on what your sql script does.
-Write-Host "Database populated..."
-popd
+function Successful-Step {
+  process { Write-Host $_ -ForegroundColor green -BackgroundColor black }
+}
+function Error-Step {
+  process { Write-Host $_ -ForegroundColor white -BackgroundColor red }
+}
 
-#Fill connectionString template and populates
-pushd ..\lib\Configs
-
-$newValue = '"Server=' + $serviceInstance + ';Integrated Security=true;Connect Timeout=5;database=' + $database + '"'
-write-host $newValue
-
-Copy-item '.\connectionStrings.config' '..\..\src\Roadkill.Web\connectionStrings.config'
+$iconSuccess = "[!]"
+$iconError ="`a/!\ "
 
 
-(Get-Content '..\..\src\Roadkill.Web\connectionStrings.config') | Foreach-Object {$_ -replace '""', $newValue } | Set-Content '..\..\src\Roadkill.Web\connectionStrings.config'
-Write-Host "Connectionstring created..."
-popd
+# Database Creation
+try {
+	Write-Host "`r`n> Creating database...`r`n"
+
+	$srv = new-Object Microsoft.SqlServer.Management.Smo.Server($serviceInstance)
+	$db = New-Object Microsoft.SqlServer.Management.Smo.Database($srv, $database)
+	$db.Create()
+	Write-Host $db.CreateDate
+}
+
+# DB creation Error
+catch { "$iconError An error occured in $database database creation.`r`n" | Error-Step };
+
+# DB creation Success
+if (!$error) {
+"$iconSuccess $database database created`r`n" | Successful-Step
+
+	# Execute sql initial script
+	try {
+		pushd ..\lib\Test-databases
+		Invoke-sqlcmd -inputfile ".\roadkill-sqlserver.sql" -serverinstance $serviceInstance -database $database # the parameter -database can be omitted based on what your sql script does.
+		popd
+		
+	}
+	
+	# SQL initial script Fails
+	catch {"$iconError Isn't possible to populate $database`r`n" | Error-Step };
+	
+	# SQL initial script Success
+	if (!$error) {
+		"$iconSuccess Database populated`r`n" | Successful-Step
+		
+		#Fill connectionString template and populates
+		try {
+			
+			pushd ..\lib\Configs
+
+			$newValue = '"Server=' + $serviceInstance + ';Integrated Security=true;Connect Timeout=5;database=' + $database + '"'
+			write-host $newValue `r`n | Successful-Step
+
+			Copy-item '.\connectionStrings.config' '..\..\src\Roadkill.Web\connectionStrings.config'
+
+
+			(Get-Content '..\..\src\Roadkill.Web\connectionStrings.config') | Foreach-Object {$_ -replace '""', $newValue } | Set-Content '..\..\src\Roadkill.Web\connectionStrings.config'
+			Write-Host "$iconSuccess connectionString created... `r`n" | Successful-Step
+			popd
+			
+		}
+		
+		#Fill connectionString templatecreation error
+		catch {"$iconError Isn't possible to create connectionString`r`n" | Error-Step };
+			
+		if (!$error) {
+			"$iconSuccess connectionString created `n Open .sln now.`r`n" | Successful-Step | start ..\Roadkill.sln
+		}
+	}
+}
+#ii install.log
